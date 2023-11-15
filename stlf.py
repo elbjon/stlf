a626311787d6a3ea5615deeff30da558c6053aca
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-15 23:44:17 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -30,15 +30,14 @@ m
 
 
 # center on Liberty Bell, add marker
-#m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-#folium.Marker(
-#    [39.949610, -75.150282], popup="HELLO", tooltip="HELLOHELLO"
-#).add_to(m)
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="HELLO", tooltip="HELLOHELLO"
+).add_to(m)
 
 # call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
 
-try:
-    st_data = st_folium(m, width=725)
-except:
-    m
+st_data
+
 folium_static(m)
