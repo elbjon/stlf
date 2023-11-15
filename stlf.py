6751d1dbdb694cbe84ec531949938a0783697f93
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-15 18:49:06 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -6,3 +6,38 @@ import folium
 
 
 st.write('hello world')
+
+# Create a map using the Map() function and the coordinates for Boulder, CO
+m = folium.Map(location=[52.8, 10.8], tiles="cartodb positron")
+
+# Display the map
+m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="HELLO", tooltip="HELLOHELLO"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+
+try:
+    st_data = st_folium(m, width=725)
+except:
+    m
