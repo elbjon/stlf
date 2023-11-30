bdc17947223020cb20efa961f611e62dd1e36443
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-11-30 13:24:32 +0100

    NextTry

--- stlf.py
+++ stlf.py
@@ -4,7 +4,10 @@ import folium
 from ast import literal_eval
 from streamlit_folium import st_folium, folium_static
 from folium.plugins import Geocoder
+from folium.plugins import Draw
 import json
+import os
+
 st.set_page_config(layout="wide")
 
 
@@ -186,6 +189,74 @@ elif choice == "Detail Map":
 st.write(' Is this the end?')
 
 
+m = folium.Map(tiles=None)
+folium.TileLayer("OpenStreetMap").add_to(m)
+folium.TileLayer("cartodb positron",show=False).add_to(m)
+folium.TileLayer(
+            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr = 'Esri',
+            name = 'Esri Satellite',
+            show=False,
+            overlay = False,
+            control = True
+           ).add_to(m)
+
+folium.TileLayer(
+    tiles = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    attr='opentopomap.org',
+    show=False,
+    ).add_to(m)
+
+# Add a drawing tool to the map
+draw = Draw(export=True)
+draw.add_to(m)
+
+# Display the map using Streamlit
+
+
+
+
+# add image
+merc = '1.png' #= os.path.join("data", "Mercator_projection_SW.png")
+if not os.path.isfile(merc):
+    print(f"Could not find {merc}")
+else:
+    img = folium.raster_layers.ImageOverlay(
+        name="Mercator projection SW",
+        image=merc,
+        bounds=[[51.85, 9.6], [53.3, 11.70]],
+        opacity=0.6,
+        interactive=True,
+        cross_origin=False,
+        zindex=1,
+    )
+    
+    #folium.Popup("I am an image").add_to(img)
+    
+    img.add_to(m)
+    folium.LayerControl().add_to(m)
+    
+    folium.Marker(
+        [52, 10], popup="52,10", tooltip="HELLOHELLO"
+    ).add_to(m)
+    folium.Marker(
+        [53, 10], popup="53, 10", tooltip="HELLOHELLO"
+    ).add_to(m)
+    folium.Marker(
+        [53, 11], popup="53, 11", tooltip="HELLOHELLO"
+    ).add_to(m)
+    folium.Marker(
+        [52, 11], popup="52, 11", tooltip="HELLOHELLO"
+    ).add_to(m)
+
+
+
+folium.LayerControl().add_to(m)
+
+
+
+# Display the drawn features
+st.write(draw)
 
 
 
