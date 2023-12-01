3c116b777d112c366ffb505206e466d77e2a2ac4
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-02 00:17:11 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -1,66 +1,55 @@
 import os
 import streamlit as st
 import pandas as pd
-import streamlit.components.v1 as components
+from PIL import Image
 
-# Function to load image names from subfolder into a DataFrame
-def load_image_names(subfolder):
-    image_names = os.listdir(subfolder)
-    df = pd.DataFrame({'Image': image_names, 'preselect': 0, 'notes': ''})
-    return df
+# Define the path to the image folder
+repo_path = 'path/to/your/github/repo'
+img_folder = os.path.join(repo_path, 'img')
 
-# Function to display images and handle selection
-def display_images(images_df, current_index):
-    st.image(images_df.iloc[current_index]['Image'], use_column_width=True, width=90, caption=images_df.iloc[current_index]['Image'])
+# Get a list of subfolders in the img directory
+subfolders = [f.name for f in os.scandir(img_folder) if f.is_dir()]
 
-    # Checkbox for preselecting images
-    preselect = st.checkbox('Preselect', value=bool(images_df.iloc[current_index]['preselect']))
+# Initialize a DataFrame to store image names, preselect status, and notes
+data = {'Image': [], 'Preselect': [], 'Note': []}
+df = pd.DataFrame(data)
 
-    # Save the preselection state
-    images_df.at[current_index, 'preselect'] = 1 if preselect else 0
+# Populate the DataFrame with image names from subfolders
+for subfolder in subfolders:
+    subfolder_path = os.path.join(img_folder, subfolder)
+    image_names = [f.name for f in os.scandir(subfolder_path) if f.is_file() and f.name.endswith(('.jpg', '.png'))]
+    df = pd.concat([df, pd.DataFrame({'Image': image_names, 'Preselect': 0, 'Note': ''})], ignore_index=True)
 
-# Main Streamlit app
-def main():
-    st.title("Image Selection App")
+# Define Streamlit app layout
+st.title("Image Selector App")
 
-    # List all subfolders in the 'img' directory
-    subfolders = [f.path for f in os.scandir('img') if f.is_dir()]
+# Display images in seven columns
+columns = st.beta_columns(7)
+for i, col in enumerate(columns):
+    if i < len(df):
+        img_path = os.path.join(img_folder, df.loc[i, 'Image'])
+        img = Image.open(img_path)
+        col.image(img, use_column_width=True, caption=df.loc[i, 'Image'])
 
-    # Sidebar to select subfolder
-    selected_subfolder = st.sidebar.selectbox("Select Subfolder", subfolders)
+        # Checkbox for image selection
+        selected = col.checkbox("Select", key=f"select_{i}")
+        if selected:
+            df.loc[i, 'Preselect'] = 1
+        else:
+            df.loc[i, 'Preselect'] = 0
 
-    # Load images into DataFrame
-    images_df = load_image_names(selected_subfolder)
+# Back and Next buttons
+back_pressed = st.button("Back")
+next_pressed = st.button("Next")
 
-    # Display columns of images
-    num_columns = 7
-    num_images = len(images_df)
-    images_per_column = num_images // num_columns
+# Handle button clicks
+if back_pressed:
+    st.write("Back button clicked")
+    # Add logic to go back to the previous set of images
 
-    for col in range(num_columns):
-        start_index = col * images_per_column
-        end_index = (col + 1) * images_per_column if col < num_columns - 1 else num_images
-        column_images_df = images_df.iloc[start_index:end_index]
+if next_pressed:
+    st.write("Next button clicked")
+    # Add logic to go to the next set of images
 
-        # Display images in this column
-        with st.container():
-            st.write(f"Column {col + 1}")
-            for index, image_row in column_images_df.iterrows():
-                display_images(images_df, index)
-
-    # 'Back' and 'Next' buttons
-    current_index = st.session_state.get('current_index', 0)
-    back_button = st.button("Back")
-    next_button = st.button("Next")
-
-    if back_button:
-        current_index = max(0, current_index - 1)
-    elif next_button:
-        current_index = min(num_images - 1, current_index + 1)
-
-    # Save current index in session state
-    st.session_state.current_index = current_index
-
-# Run the app
-if __name__ == '__main__':
-    main()
+# Display the DataFrame (optional, for debugging)
+st.write(df)
