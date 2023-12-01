ec766f9139a4dbb06b47989fdbecb73a51993e1b
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-02 00:22:13 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -3,40 +3,32 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-# Define the path to the image folder
-repo_path = 'path/to/your/github/repo'
-img_folder = os.path.join(repo_path, 'img')
+# Select a specific subfolder
+selected_subfolder = '52N10E'
+subfolder_path = os.path.join('img', selected_subfolder)
 
-# Get a list of subfolders in the img directory
-subfolders = [f.name for f in os.scandir(img_folder) if f.is_dir()]
+# Get the list of image names in the selected subfolder
+image_names = [f.name for f in os.scandir(subfolder_path) if f.is_file() and f.name.endswith(('.jpg', '.png'))]
 
 # Initialize a DataFrame to store image names, preselect status, and notes
-data = {'Image': [], 'Preselect': [], 'Note': []}
+data = {'Image': image_names, 'Preselect': [0] * len(image_names), 'Note': [''] * len(image_names)}
 df = pd.DataFrame(data)
-
-# Populate the DataFrame with image names from subfolders
-for subfolder in subfolders:
-    subfolder_path = os.path.join(img_folder, subfolder)
-    image_names = [f.name for f in os.scandir(subfolder_path) if f.is_file() and f.name.endswith(('.jpg', '.png'))]
-    df = pd.concat([df, pd.DataFrame({'Image': image_names, 'Preselect': 0, 'Note': ''})], ignore_index=True)
 
 # Define Streamlit app layout
 st.title("Image Selector App")
 
-# Display images in seven columns
-columns = st.beta_columns(7)
-for i, col in enumerate(columns):
-    if i < len(df):
-        img_path = os.path.join(img_folder, df.loc[i, 'Image'])
-        img = Image.open(img_path)
-        col.image(img, use_column_width=True, caption=df.loc[i, 'Image'])
+# Display images in a single column
+for i in range(len(df)):
+    img_path = os.path.join(subfolder_path, df.loc[i, 'Image'])
+    img = Image.open(img_path)
+    st.image(img, use_column_width=True, caption=df.loc[i, 'Image'])
 
-        # Checkbox for image selection
-        selected = col.checkbox("Select", key=f"select_{i}")
-        if selected:
-            df.loc[i, 'Preselect'] = 1
-        else:
-            df.loc[i, 'Preselect'] = 0
+    # Checkbox for image selection
+    selected = st.checkbox("Select", key=f"select_{i}")
+    if selected:
+        df.loc[i, 'Preselect'] = 1
+    else:
+        df.loc[i, 'Preselect'] = 0
 
 # Back and Next buttons
 back_pressed = st.button("Back")
