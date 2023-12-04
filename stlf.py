1d65b2eafe27f324a2a8bba081773a4f4ea8db19
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-04 17:15:14 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -61,100 +61,102 @@ if st.session_state['loc_chosen']==0:
         #st.button('next')              
 
         st.session_state['loc_chosen'] = data
+else:
+    st.write('test my patience')
 
     
 
-        st.write('this is a test if condition is tested here')
+    st.write('this is a test if condition is tested here')
 
 
 
-        # List all subfolders in the 'img' directory
-        subfolders = [f.path for f in os.scandir('img') if f.is_dir()]
+    # List all subfolders in the 'img' directory
+    subfolders = [f.path for f in os.scandir('img') if f.is_dir()]
 
 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #st.sidebar.markdown(scrollable_content, unsafe_allow_html=True)
+    selected_subfolder = 'img/52N10E'
+
+    subfolder_path = selected_subfolder #os.path.join('img', selected_subfolder)
+
+
+
+    # Get the list of image names in the selected subfolder
+    image_names = [f.name for f in os.scandir(subfolder_path) if f.is_file() and f.name.endswith(('.jpg', '.png'))] # change accordingly
+
+    # Initialize a DataFrame to store image names, preselect status, and notes
+    data = {'Image': image_names, 'Preselect': [0] * len(image_names), 'Note': [''] * len(image_names)}
+
+
+
+
+
+
+
+
+    # Check if 'df' is not in session state, and if not, store it
+    if 'df' not in st.session_state:
+        st.session_state['df'] = pd.DataFrame(data)
+
+    # Define Streamlit app layout
+    st.title("Part 2: Image Selector App")
+
+
+    for i, v in enumerate(st.session_state.df['Image']):
+
+        img_path = os.path.join(subfolder_path, v)#st.session_state.df.loc[i, 'Image'])
+        #st.sidebar.markdown(img_path)
+        img = Image.open(img_path)#.resize((150, 200))
         
+        st.sidebar.image(img, use_column_width=True) #, caption='Your Image'
+
+        # Checkbox for image selection in the sidebar
+        selected = st.sidebar.checkbox(f"Select Image {i}", key=f"select_{i}", value=st.session_state.df.loc[i, 'Preselect'])
+        st.session_state.df.loc[i, 'Preselect'] = int(selected)
 
 
 
+        if selected:
+            st.session_state.df.loc[i, 'Preselect'] = 1
+        #else:
+        #    st.session_state.df.loc[i, 'Preselect'] = 0
+        #col_images[col].image(img, use_column_width=True, caption=st.session_state.df.loc[i, 'Image'])
 
+            # Checkbox for image selection
+        #    selected = col_images[col].checkbox("Select", key=f"select_{i}")
+            #if selected:
+            #   st.session_state.df.loc[i, 'Preselect'] = 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #st.sidebar.markdown(scrollable_content, unsafe_allow_html=True)
-        selected_subfolder = 'img/52N10E'
-
-        subfolder_path = selected_subfolder #os.path.join('img', selected_subfolder)
-
-
-
-        # Get the list of image names in the selected subfolder
-        image_names = [f.name for f in os.scandir(subfolder_path) if f.is_file() and f.name.endswith(('.jpg', '.png'))] # change accordingly
-
-        # Initialize a DataFrame to store image names, preselect status, and notes
-        data = {'Image': image_names, 'Preselect': [0] * len(image_names), 'Note': [''] * len(image_names)}
-
-
-
-
-
-
-
-
-        # Check if 'df' is not in session state, and if not, store it
-        if 'df' not in st.session_state:
-            st.session_state['df'] = pd.DataFrame(data)
-
-        # Define Streamlit app layout
-        st.title("Part 2: Image Selector App")
-
-
-        for i, v in enumerate(st.session_state.df['Image']):
-
-            img_path = os.path.join(subfolder_path, v)#st.session_state.df.loc[i, 'Image'])
-            #st.sidebar.markdown(img_path)
-            img = Image.open(img_path)#.resize((150, 200))
-            
-            st.sidebar.image(img, use_column_width=True) #, caption='Your Image'
-
-            # Checkbox for image selection in the sidebar
-            selected = st.sidebar.checkbox(f"Select Image {i}", key=f"select_{i}", value=st.session_state.df.loc[i, 'Preselect'])
-            st.session_state.df.loc[i, 'Preselect'] = int(selected)
-
-
-
-            if selected:
-                st.session_state.df.loc[i, 'Preselect'] = 1
-            #else:
-            #    st.session_state.df.loc[i, 'Preselect'] = 0
-            #col_images[col].image(img, use_column_width=True, caption=st.session_state.df.loc[i, 'Image'])
-
-                # Checkbox for image selection
-            #    selected = col_images[col].checkbox("Select", key=f"select_{i}")
-                #if selected:
-                #   st.session_state.df.loc[i, 'Preselect'] = 1
-
-        st.write(st.session_state.df)
+    st.write(st.session_state.df)
 
