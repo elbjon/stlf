c7e12e0044f30ad5715943a968e25ddaa82fcdac
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-05 10:41:01 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -112,6 +112,7 @@ def populate_side(p, subfolder_path):
 
 
 def add_image_overlay(p, img_path):
+    st.write(img_path)
     bounds=[[st.session_state['loc_chosen'][0]-0.15,st.session_state['loc_chosen'][1]-0.4],[st.session_state['loc_chosen'][0]+1.3,st.session_state['loc_chosen'][1]+1.7]]
     #st.write(str(bounds))
     img_overlay = folium.raster_layers.ImageOverlay(
