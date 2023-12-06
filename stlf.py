3c115f5c4ad9b4b23eccb1f812c796a79125ffc4
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-06 11:57:00 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -216,7 +216,7 @@ def map_detail(subfolder_path):
     #print(f"Row {index + 1}: Column1 = {row['Column1']}, Column2 = {row['Column2']}, Column3 = {row['Column3']}")
     
     
-        st.markdown(f"[URL_IMG_{row['No']}](www.url.com)")
+        st.markdown(f'Image {row['No']} ', f'{row['Image']}', f"[URL_IMG_{row['No']}](www.url.com)")
 
     # no return needed, map printing done here. delete return when everything is running
     return p
