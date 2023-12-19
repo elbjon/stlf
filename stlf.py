2d005d19b17cc06eb1bc549c7d34b3200ff8830a
Jonas Elbers <143721070+elbjon@users.noreply.github.com>
2023-12-19 10:37:47 +0100

    Update stlf.py

--- stlf.py
+++ stlf.py
@@ -42,15 +42,25 @@ def get_pos(lat, lng):
 
 def zeropoint_coord(data):
     return_data = []
-    for i, v in enumerate(data):
-        st.write(i,v, len(data))
-        if v >= 0:
-            return_data.append(int(v))
-        else:
-            return_data.append(int(v-1))
-        st.write('MOST IMPORTANT: ',v, 'becomes: ', return_data[i])
-        #data=tuple(return_data)
-        #st.write('tuple data', data)
+    return_string ='' #subfolder
+    if data[0] >= 0:
+        return_string = return_string + str(data[0])+'N_'
+    else:
+        return_string = return_string + str(data[0])+'S_'
+    st.write(return_string)
+
+    #for i, v in enumerate(data):
+        
+        
+        
+        
+    #    if v >= 0:
+    #        return_data.append(int(v))
+    #    else:
+    #        return_data.append(int(v-1))
+    #    st.write('MOST IMPORTANT: ',v, 'becomes: ', return_data[i])
+    #    data=tuple(return_data)
+    #    st.write('tuple data', data)
    
     #st.session_state['loc_chosen'] = int(data)
 
